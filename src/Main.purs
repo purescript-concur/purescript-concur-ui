99cee763ad05f7dab0e6232c7c910802a466d1e3
Anupam Jain <ajnsit@gmail.com>
2020-04-04 12:03:52 +0530

    Add some interactivity - a checkbox widget to the main page

--- src/Main.purs
+++ src/Main.purs
@@ -2,17 +2,22 @@ module Main where
 
 import Prelude
 
-import Concur.Core (Widget)
+import Concur.Core.Types (Widget)
 import Concur.React (HTML)
+import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
+import Data.Int (toNumber)
 import Effect (Effect)
 import Element (Element)
 import Element as E
 import Element.Background as Background
+import Element.Border as Border
+import Internal.Model as X
+import Util (zeroDiv)
 
 
 main :: Effect Unit
-main = runWidgetInDom "root" view
+main = runWidgetInDom "root" (E.layout [] view)
 
 type Layout =
   { bottom :: Int
@@ -30,15 +35,14 @@ edges =
     }
 
 
-view :: forall a. Widget HTML a
+view :: forall a. Element a
 view =
-  E.layout [ E.inFront header ] $
-    E.column [ E.width E.fill ]
-      [ header
-      , hero
-      , content
-      , footer
-      ]
+    E.el [ E.inFront header ] $
+      E.column [ E.width E.fill ]
+        [ hero
+        , content
+        , footer
+        ]
 
 header :: forall a. Element a
 header =
@@ -48,13 +52,15 @@ header =
         , E.el [] (E.text "About Us")
         , E.el [] (E.text "Services")
         , E.el [] (E.text "Contact Us")
+        , E.el [] (X.unstyled myCheckbox)
         ]
 
 
 hero :: forall a. Element a
 hero =
     E.row [ E.width E.fill, Background.color (E.rgb 0.0 0.7 0.0) ]
-        [ E.image [ E.width E.fill ]
+        [ E.none
+        , E.image [ E.width E.fill ]
             { src: "https://via.placeholder.com/1400x400"
             , description: "Hero Image"
             }
@@ -140,3 +146,55 @@ contactUs =
 footer :: forall a. Element a
 footer =
     E.row [ E.height (E.px 200), E.width E.fill, Background.color (E.rgb 0.0 0.3 0.0) ] []
+
+toggleCheckboxWidget :: { offColor :: E.Color, onColor :: E.Color, sliderColor :: E.Color, toggleWidth :: Int, toggleHeight :: Int } -> Boolean -> Element Unit
+toggleCheckboxWidget { offColor, onColor, sliderColor, toggleWidth, toggleHeight } checked =
+  E.el
+    [ Background.color $ if checked then onColor else offColor
+    , E.width $ E.px toggleWidth
+    , E.height $ E.px toggleHeight
+    , Border.rounded 14
+    , unit <$ E.htmlAttribute (P.onClick)
+    , E.inFront $
+        E.el [ E.height E.fill ] $
+            E.el
+                [ Background.color sliderColor
+                , Border.rounded $ sliderSize `zeroDiv` 2
+                , E.width $ E.px sliderSize
+                , E.height $ E.px sliderSize
+                , E.centerY
+                , E.moveRight (toNumber pad)
+                , E.htmlAttribute $ P.style
+                    { transition: ".4s"
+                    , transform: if checked then "translateX(" <> translation <> "px)" else ""
+                    }
+                ]
+                (E.text "")
+    ]
+    (E.text "")
+  where
+    pad = 3
+    sliderSize = toggleHeight - 2 * pad
+    translation = show (toggleWidth - sliderSize - pad)
+
+myCheckbox :: forall a. Widget HTML a
+myCheckbox = go false
+  where
+    go b = do
+      myCheckboxView b
+      go (not b)
+
+myCheckboxView :: Boolean -> Widget HTML Unit
+myCheckboxView b = E.layoutInner [] $
+  toggleCheckboxWidget opts b
+  where
+    opts =
+      { offColor: lightGrey
+      , onColor: green
+      , sliderColor: white
+      , toggleWidth: 60
+      , toggleHeight: 28
+      }
+    lightGrey = E.rgb255 187 187 187
+    green = E.rgb255 39 203 139
+    white = E.rgb255 255 255 255
